6a096ef63e38bc91a56dc87c57d0d7745fb62d3f
KaterynaKovtun <katebidnyk@yahoo.com>
2020-04-12 23:14:41 +0300

    slider

--- js/index.js
+++ js/index.js
@@ -8,4 +8,35 @@ $('.filter-button-group').on( 'click', 'button', function() {
   var filterValue = $(this).attr('data-filter');
   $grid.isotope({ filter: filterValue });
 });
+//slick-slider
 
+$(document).ready(function(){
+  $('.single-item').slick();
+});
+
+
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
